8699addfe8de2dd9e5e684e3d5ecdc97cef18a4f
Guille <guillenotfound@gmail.com>
2023-02-19 19:43:45 +0100

    chore: add some plugins

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -15,6 +15,8 @@ opt.clipboard = 'unnamedplus'
 
 vim.keymap.set('i', '<C-d>', '<Del>')
 
+vim.keymap.set('n', '<leader>b', ':NeoTreeFocusToggle<CR>')
+
 local au_ft = vim.api.nvim_create_augroup("ft", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "Jenkinsfile" },
