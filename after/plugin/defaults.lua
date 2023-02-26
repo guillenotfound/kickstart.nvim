7d4428cdd43e904ba5917a6d63b6659a116001a9
Guille <guillenotfound@gmail.com>
2023-02-26 11:05:19 +0100

    feat: adds some keymaps

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -13,9 +13,67 @@ opt.autoindent = true
 -- clipboard
 opt.clipboard = 'unnamedplus'
 
+opt.scrolloff = 8
+
+opt.splitright = true
+opt.splitbelow = true
+
+-- Insert mode --
+-- allow forward deleting
 vim.keymap.set('i', '<C-d>', '<Del>')
 
-vim.keymap.set('n', '<leader>b', ':NeoTreeFocusToggle<CR>')
+vim.keymap.set("i", "jk", "<ESC>")
+
+
+-- Normal mode --
+-- Keep cursor in the middle when navigating/searching
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("n", "<leader>|", "<C-w>v")
+vim.keymap.set("n", "<leader>-", "<C-w>s")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Start search and replace with word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- disable arrow keys to learn hjkl
+vim.keymap.set("n", "<up>", "<Nop>")
+vim.keymap.set("n", "<down>", "<Nop>")
+vim.keymap.set("n", "<left>", "<Nop>")
+vim.keymap.set("n", "<right>", "<Nop>")
+
+
+-- Visual mode --
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==")
+
+vim.keymap.set("v", "p", '"_dP')
+
+
+-- Visual Block --
+-- Move text up and down
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv")
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv")
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv")
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv")
+
+
+
+vim.keymap.set('n', '<leader>b', ':NeoTreeRevealToggle<CR>')
+
+vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
 
 local au_ft = vim.api.nvim_create_augroup("ft", { clear = true })
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
